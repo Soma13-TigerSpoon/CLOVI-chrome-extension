e72d10cf63c4f724476f60b1973aed8bc7c9f7e7
voka <chdlswhd7@naver.com>
2022-07-11 01:26:54 +0900

    Feat: aws Lamdba 함수에서 데이터 받아오는 기능 추가
    Workflow:
    1. 사용자가 클릭한 탭이 youtube 링크인지 확인
    2. youtube 링크에서 video_id 추출
    3. video_id를 파라미터로 Lamdba 함수에 Get 요청
    
    수정이 필요한 사항:
    1. 사용자가 chrome extension을 클릭했을때 기능이 실행되도록 변경 필요
    2. youtube 링크 중에서도 영상이 나오는 페이지에 있는지 확인 필요
    3. 현재는 "핏더사이즈"를 default creator로 했지만 추후에는 사용자가 보고있는 페이지의 html을 이용해서 크리에이터 명을 가져와야 함

--- src/service/background.js
+++ src/service/background.js
@@ -1,9 +1,25 @@
-chrome.tabs.onActivated.addListener(move);
+import Video from './video.js';
+chrome.tabs.onActivated.addListener(getUrl);
 
-function move() {
+function getUrl() {
+  let url,video_id;
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) {
-    var url = tabs[0].url;
+    url = tabs[0].url;
+    video_id = youtube_id_parser(url)
     console.log(url);
+    if (video_id != false){
+      console.log(video_id);
+      const myvideo = new Video(video_id,"핏더사이즈");
+      const response = myvideo.info();
+      console.log(response);
+    }
   });
   
+
+      
+}
+function youtube_id_parser(url){
+  var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+  var match = url.match(regExp);
+  return (match&&match[7].length==11)? match[7] : false;
 }

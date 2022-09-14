34245df7005dc034b027f327ad0e840ba69b0ab5
khhan <jacob2572502@gmail.com>
2022-09-14 21:48:49 +0900

    Chore: made classnames unique
    익스텐션의 CSS가 기존youtube 웹페이지 상의 다른 element에게 영향을 주지 않도록 기존 classname에는 clv 키워드를 추가하고, div와 main과 같은 실제 tagname을 사용하지 않도록 했다.
    
    Resolves: NTSP-84

--- src/content-script/content-script.js
+++ src/content-script/content-script.js
@@ -9,16 +9,18 @@ const $clovi = document.createElement("div");
 $clovi.id = "clovi";
 $body.appendChild($clovi);
 const $header = document.createElement("header");
+$header.className = "clv-header"
 const $main = document.createElement("main");
-$main.innerHTML = '<div class="emptyItems">No items to load.</div>';
+$main.className = "main"
+$main.innerHTML = '<div class="clv-div cmain clv-emptyItems">No items to load.</div>';
 const $header__youtuber = document.createElement("div");
 const $header__right = document.createElement("div");
 const $right__model = document.createElement("div");
 const $right__close = document.createElement("div");
-$header__youtuber.className = "header__youtuber";
-$header__right.className = "header__right";
-$right__model.className = "right__model";
-$right__close.className = "right__close";
+$header__youtuber.className = "clv-div clv-header__youtuber";
+$header__right.className = "clv-div clv-header__right";
+$right__model.className = "clv-div clv-right__model";
+$right__close.className = "clv-div clv-right__close";
 $right__close.innerHTML = "<span>&#10005;</span>";
 $right__close.addEventListener("click", () => {
   $clovi.style.display = "none";
@@ -51,11 +53,11 @@ const render = (video_data=null) => {
       $main.innerHTML = get_items(currentItemId);
 
       if (newCurrentItemId !== -1) {
-        let $folds = document.getElementsByClassName("fold");
+        let $folds = document.getElementsByClassName("clv-fold");
         for (let i = 0; i < $folds.length; i++) {
           $folds[i].addEventListener("click", function () {
-            this.classList.toggle("active");
-            this.children[0].classList.toggle("superactive");
+            this.classList.toggle("clv-active");
+            this.children[0].classList.toggle("clv-superactive");
             let shops = this.parentElement.nextElementSibling;
             if (shops.style.display === "block") {
               shops.style.display = "none";
@@ -70,8 +72,8 @@ const render = (video_data=null) => {
 
   const get_header__youtuber = (youtuber) => {
     return `
-      <img class="header__youtuber__img" src=${youtuber.profileUrl}>
-      <div>${youtuber.creator}</div>
+      <img class="clv-header__youtuber__img" src=${youtuber.profileUrl}>
+      <div class="clv-div">${youtuber.creator}</div>
     `;
   };
   const get_right__model = (currentItemId) => {
@@ -80,15 +82,15 @@ const render = (video_data=null) => {
     } else {
       let model = collections[currentItemId].model;
       return `
-        <div class="right__model-name right__model__children">${model.name}</div>
-        <div class="right__model-height right__model__children">${model.height}cm</div>
-        <div class="right__model-weight right__model__children">${model.weight}kg</div>
+        <div class="clv-div clv-right__model-name clv-right__model__children">${model.name}</div>
+        <div class="clv-div clv-right__model-height clv-right__model__children">${model.height}cm</div>
+        <div class="clv-div clv-right__model-weight clv-right__model__children">${model.weight}kg</div>
       `;
     }
   };
   const get_items = (clothes, reset) => {
     if (currentItemId === -1) {
-      return '<div class="emptyItems">No items to load.</div>';
+      return '<div class="clv-div clv-emptyItems">No items to load.</div>';
     } else {
       console.log(collections);
       let clothes = collections[currentItemId].items;
@@ -96,26 +98,26 @@ const render = (video_data=null) => {
       return clothes
         .map(
           (i) => `
-          <div class="card">
-            <a href="${i.shops[0].shopUrl}" target="_blank">
-              <div class="item">
-                  <img class="item__img" src=${i.itemImgUrl}>
-                  <div class="item__info">
-                      <div class="info__name">${i.name}</div>
-                      <div class="info__others">
-                          <div class="others__seller">
-                              <img class="seller__img" src=${
+          <div class="clv-div clv-card">
+            <a class="clv-a" href="${i.shops[0].shopUrl}" target="_blank">
+              <div class="clv-div clv-item">
+                  <img class="clv-item__img" src=${i.itemImgUrl}>
+                  <div class="clv-div clv-item__info">
+                      <div class="clv-div clv-info__name">${i.name}</div>
+                      <div class="clv-div clv-info__others">
+                          <div class="clv-div clv-others__seller">
+                              <img class="clv-seller__img" src=${
                                 i.shops[0].logoUrl
                               }>
-                              <div class="seller__name">${
+                              <div class="clv-div clv-seller__name">${
                                 i.shops[0].name
                               }</div>
                           </div>
-                          <div class="others__right">
-                              <div class="right__price">${
+                          <div class="clv-div clv-others__right">
+                              <div class="clv-div clv-right__price">${
                                 new Intl.NumberFormat().format(i.shops[0].price)
                               }원</div>
-                              <div class="right__colorSize">${i.color}/${
+                              <div class="clv-div clv-right__colorSize">${i.color}/${
             i.size
           }</div>
                           </div>
@@ -123,21 +125,21 @@ const render = (video_data=null) => {
                   </div>
               </div>
             </a/>
-            <div class="fold">
-                <div class="fold__button">&#9660;</div>
+            <div class="clv-div clv-fold">
+                <div class="clv-div clv-fold__button">&#9660;</div>
             </div>
         </div>
-        <div class="shops">
+        <div class="clv-div clv-shops">
           ${i.shops
             .map(
               (shop) => `
-              <a href="${shop.shopUrl}" target="_blank">
-                <div class="shops__shop">
-                  <div class="shop__shopInfo">
-                    <img class="shopInfo__shopLogo" src=${shop.logoUrl}>
-                    <div class="shopInfo__shopName">${shop.name}</div>
+              <a class="clv-a" href="${shop.shopUrl}" target="_blank">
+                <div class="clv-div clv-shops__shop">
+                  <div class="clv-div clv-shop__shopInfo">
+                    <img class="clv-shopInfo__shopLogo" src=${shop.logoUrl}>
+                    <div class="clv-div clv-shopInfo__shopName">${shop.name}</div>
                   </div>
-                  <div class="shop__price">${new Intl.NumberFormat().format(shop.price)}원</div>
+                  <div class="clv-div clv-shop__price">${new Intl.NumberFormat().format(shop.price)}원</div>
                 </div>
               </a>
           `

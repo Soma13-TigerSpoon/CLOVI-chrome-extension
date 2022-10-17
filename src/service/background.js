dbbf4a70bf339125bfa18cbfe675bdfd5b313196
khhan <jacob2572502@gmail.com>
2022-10-17 11:25:16 +0900

    Publish: 크롬 공식 문서에 맞게 이거저거 수정함 Resolves: NTSP-56

--- src/service/background.js
+++ src/service/background.js
@@ -7,39 +7,39 @@ function youtube_id_parser(url) {
   return match && match[7].length == 11 ? match[7] : false;
 }
 
-async function getVideoData(request, sender) {
-  console.log(
-    sender.tab
-      ? "from a content script:" + sender.tab.url
-      : "from the extension"
-  );
+// async function getVideoData(request, sender) {
+//   console.log(
+//     sender.tab
+//       ? "from a content script:" + sender.tab.url
+//       : "from the extension"
+//   );
 
-  let url, video_id, video_data;
-  url = sender.tab.url;
-  video_id = youtube_id_parser(url);
-  console.log(url);
-  if (video_id != false) {
-    console.log(video_id);
-    console.log(typeof video_id);
+//   let url, video_id, video_data;
+//   url = sender.tab.url;
+//   video_id = youtube_id_parser(url);
+//   console.log(url);
+//   if (video_id != false) {
+//     console.log(video_id);
+//     console.log(typeof video_id);
 
-    const storageObj = await chrome.storage.local.get(null);
-    console.log(storageObj);
+//     const storageObj = await chrome.storage.local.get(null);
+//     console.log(storageObj);
 
-    if (!storageObj.hasOwnProperty(video_id)) {
-      console.log("first time to load this video. setting storage", video_id);
-      const myvideo = new Video(video_id);
-      const newVideoData = await myvideo.info();
-      await chrome.storage.local.set({ [video_id]: newVideoData.data });
-    }
+//     if (!storageObj.hasOwnProperty(video_id)) {
+//       console.log("first time to load this video. setting storage", video_id);
+//       const myvideo = new Video(video_id);
+//       const newVideoData = await myvideo.info();
+//       await chrome.storage.local.set({ [video_id]: newVideoData.data });
+//     }
 
-    console.log("now getting data from storage");
-    video_data = await chrome.storage.local.get(video_id);
-  } else {
-    console.log(`video_id is false! - video_id: ${video_id}`);
-  }
-  console.log(video_data);
-  return video_data[video_id];
-}
+//     console.log("now getting data from storage");
+//     video_data = await chrome.storage.local.get(video_id);
+//   } else {
+//     console.log(`video_id is false! - video_id: ${video_id}`);
+//   }
+//   console.log(video_data);
+//   return video_data[video_id];
+// }
 
 // chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 //   getVideoData(request, sender).then(sendResponse);
